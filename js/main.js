2a1c0e4211638c1feb10e808d764deaf07895216
Graham Moss <graham.moss@hotmail.com>
2017-02-15 16:10:57 -0500

    complete assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
+$(document).ready(function() {
+
+  $(".switch").click(function(){
+  //   $('button').toggleClass('on off');
+  //   $('body').toggleClass('light dark');
+  //   $('.status').text('Hey, who turned off the lights?');
+  // });
+
+  if ( $('button').attr('class') === 'on') {
+    $('button').attr('class', 'off');
+    $('body').attr('class', 'dark');
+    $('.status').text('Hey, who turned off the lights?');
+
+  } else {
+
+      $('button').attr('class', 'on');
+      $('body').attr('class', 'light');
+      $('.status').text("It's so bright in here!");
 
 
+
+  }
+});
+});
